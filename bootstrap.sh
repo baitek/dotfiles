3febb83131e743a18c2eaf712dc9e3bbc9a75847
baitek <bartosz.chajdas@gmail.com>
2023-09-17 04:59:46 +0200

    Check which files are added/changed

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "${BASH_SOURCE}")"
-read -p "This will overwrite you home config. Are you sure? (y/n): " -n 1
-echo ""
-echo "$REPLY"
+read -p 'This will overwrite you home config. Are you sure? (y/n): ' -n 1
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    rsync --exclude "bootstrap.sh" \
-        --exclude "README.md" \
-        --exclude ".git/" \
-        -avh . ~
+    echo -e '\n\nModified/created files: '
+    rsync --exclude 'bootstrap.sh' \
+        --exclude 'README.md' \
+        --exclude '.git/' \
+        -avhi . ~/test | rg -e '^>f' | awk '{print "- " $2} END {if (NR == 0) print "[]"}'
 fi
 
