cc50bfc55874c825ed5e4c053d9acb5bb3d7ed51
baitek <bartosz.chajdas@gmail.com>
2023-09-17 07:21:32 +0200

    Remove query parameter for core-utils curl

--- .scripts/cht.sh
+++ .scripts/cht.sh
@@ -8,12 +8,11 @@ if [[ -z $selected ]]; then
     exit 0
 fi
 
-read -p "Enter Query: " query
-
 if grep -qs "$selected" ~/.config/tmux/tmux-cht-languages; then
-    query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    read -p "Enter Query: " query
+    query=$(echo $query | tr ' ' '+')
+    tmux neww bash -c "curl -s cht.sh/$selected/$query | less"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+    tmux neww bash -c "curl -s cht.sh/$selected | less"
 fi
 
