7f6b971480d67056775bb742c477a4a8114fc705
baitek <bartosz.chajdas@gmail.com>
2024-02-08 17:07:43 +0100

    Support completion for Neovim cmdline

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -1,8 +1,23 @@
 return {
   {
-    -- Get completion candidates from LSPs to nvim-cmp
+    -- Source for nvim-cmp's LSP suggestions
     -- See `:help cmp-nvim-lsp`
     'hrsh7th/cmp-nvim-lsp',
+  },
+  {
+    -- Source for buffer words
+    -- See `:help cmp-buffer`
+    'hrsh7th/cmp-buffer',
+  },
+  {
+    -- Source for filesystem paths
+    -- See `:help cmp-path`
+    'hrsh7th/cmp-path',
+  },
+  {
+    -- Source for VIM's cmdline (requires cmp-buffer to work)
+    -- See `:help cmp-cmdline`
+    'hrsh7th/cmp-cmdline',
   },
   {
     -- Snippet engine
@@ -22,15 +37,15 @@ return {
     -- See `:help nvim-cmp`
     'hrsh7th/nvim-cmp',
     config = function()
+      local cmp = require 'cmp'
+      local luasnip = require 'luasnip'
+      require('luasnip.loaders.from_vscode').lazy_load()
+
       local has_words_before = function()
         unpack = unpack or table.unpack
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
       end
-
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      require('luasnip.loaders.from_vscode').lazy_load()
 
       cmp.setup({
         completion = {
@@ -68,13 +83,32 @@ return {
           end, { 'i', 's' }),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true })
+          ['<CR>'] = cmp.mapping({
+            i = function(fallback)
+              if cmp.visible() and cmp.get_active_entry() then
+                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+              else
+                fallback()
+              end
+            end,
+            s = cmp.mapping.confirm({ select = true }),
+            c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+          }),
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
           { name = 'luasnip' }
         }, {
           { name = 'buffer' }
+        })
+      })
+
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
         })
       })
     end
