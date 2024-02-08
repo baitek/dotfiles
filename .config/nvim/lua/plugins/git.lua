b81530a0ae6f50674b1f3a58e218094ea0e7c3e1
baitek <bartosz.chajdas@gmail.com>
2024-02-08 04:30:52 +0100

    Set keybindings for lsp, cmp and git

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,14 +1,31 @@
 return {
   {
-    "tpope/vim-fugitive"
+    -- Arbitrary git command in NeoVim
+    -- See `:help vim-fugitive`
+    'tpope/vim-fugitive'
   },
   {
-    "lewis6991/gitsigns.nvim",
+    -- Git signs with actions attributed to them
+    -- See `:help gitsigns`
+    'lewis6991/gitsigns.nvim',
     config = function()
-      require("gitsigns").setup()
+      require('gitsigns').setup {
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
 
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+          map('n', '<leader>hp', gs.preview_hunk)
+          map('n', '<leader>hr', gs.reset_hunk)
+          map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+          map('n', '<leader>hR', gs.reset_buffer)
+          map('n', '<leader>tb', gs.toggle_current_line_blame)
+          map('n', '<leader>hd', gs.diffthis)
+        end
+      }
     end
   }
 }
--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -31,7 +31,6 @@ return {
         'yamlls'
       }
     })
-
     require('neodev').setup()
 
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
@@ -59,13 +58,6 @@ return {
       capabilities = capabilities
     })
 
-    -- Global mappings
-    -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
-
     -- Use LspAttach autocommand to only map the following keys
     -- after the language server attaches to the current buffer
     vim.api.nvim_create_autocmd('LspAttach', {
@@ -79,18 +71,10 @@ return {
         vim.keymap.set({ 'n', 'v' }, '<leader>f', function()
           vim.lsp.buf.format { async = true }
         end, opts)
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<leader>wl', function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
       end,
     })
   end
