fa256173988bb1d5fdeae7cd0d5a6fd3920bc97e
baitek <bartosz.chajdas@gmail.com>
2024-02-08 17:06:36 +0100

    Exit NeoVim when Neotree is open

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -10,6 +10,7 @@ return {
   },
   config = function()
     require('neo-tree').setup {
+      close_if_last_window = true,
       filesystem = {
         filtered_items = {
           visible = true
