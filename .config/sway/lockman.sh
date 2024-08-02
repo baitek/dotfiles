d4b776ffcd53236fa04d40b4344a14004a9127b3
baitek <bartosz.chajdas@gmail.com>
2024-08-02 09:46:50 +0200

    fix lock image

--- .config/sway/lockman.sh
+++ .config/sway/lockman.sh
@@ -2,5 +2,5 @@
 swayidle \
 	timeout 300 'swaymsg "output * dpms off"' \
 	resume 'swaymsg "output * dpms on"' &
-swaylock -i ~/.config/sway/mountains.jpeg
+swaylock -i ~/.config/sway/el-capitan.jpg
 kill %%
