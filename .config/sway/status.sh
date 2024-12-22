8fe5eb6836f0699e5fb03fe63484a9f0b373c97a
baitek <bartosz.chajdas@gmail.com>
2024-12-22 01:19:22 +0100

    misc: formatting/rename

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 date_formatted=$(
-	LANG=en_us_88591
-	date "+%H:%M:%S, %A %d-%m-%Y %p"
+  LANG=en_us_88591
+  date "+%H:%M:%S, %A %d-%m-%Y %p"
 )
 status=$(awk "NR==1" /sys/class/leds/input*::capslock/brightness)
 
 if [[ "$status" == "1" ]]; then
-	echo -e "CAPS ON   $date_formatted"
+  echo -e "CAPS ON   $date_formatted"
 else
-	echo "$date_formatted"
+  echo "$date_formatted"
 fi
