78007bde3f8906310373b5c0bf4abfa9469b32c7
baitek <bartosz.chajdas@gmail.com>
2026-07-24 00:39:32 +0200

    remove lazyvim

--- .config/nvim/lua/config/keymaps.lua
+++
@@ -1,7 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-vim.keymap.set("n", "<C-w>h", "<C-w>s", { noremap = true, silent = true, desc = "Split current window horizontally" })
-vim.keymap.set({ "n", "i", "v", "c" }, "<C-z>", function()
-  vim.cmd('echo "NO"')
-end, { noremap = true, silent = false })
